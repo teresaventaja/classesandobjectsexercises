8306e5239e6dcb327a98f2f4013fed58a8a0d732
Teresa Ventaja <71657946+teresaventaja@users.noreply.github.com>
2023-11-28 20:17:09 +0000

    Initial commit

---
+++ src/classesandobjectsexercises/Classesandobjectsexercises.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classesandobjectsexercises;
+
+/**
+ *
+ * @author User
+ */
+public class Classesandobjectsexercises {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
