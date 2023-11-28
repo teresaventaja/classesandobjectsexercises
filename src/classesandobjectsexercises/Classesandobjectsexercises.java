390364f1a5df840d73b528e53ca4c6df3360316b
Teresa Ventaja <71657946+teresaventaja@users.noreply.github.com>
2023-11-28 20:20:14 +0000

    Hello World

--- src/classesandobjectsexercises/Classesandobjectsexercises.java
+++ src/classesandobjectsexercises/Classesandobjectsexercises.java
@@ -15,6 +15,7 @@ public class Classesandobjectsexercises {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world!");
     }
     
 }
